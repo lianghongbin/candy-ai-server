2b038c86d9ad1edffd38fe14e331450291b8522d
eric.liang <viruer@gmail.com>
2025-08-01 10:26:52 -0700

    feat: 移除若依官网菜单并更新项目信息
    - 删除数据库初始化脚本中的'若依官网'菜单项
    - 更新前端首页标题和描述为Candy AI相关信息
    - 更新前端package.json项目信息
    - 更新前端环境配置文件标题
    - 更新前端版本号为1.0.1
    - 替换所有若依相关链接为项目GitHub地址
    - 更新联系信息为项目技术栈信息

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -2,15 +2,15 @@
   <div class="app-container home">
     <el-row :gutter="20">
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>若依后台管理框架</h2>
+        <h2>Candy AI 虚拟女友管理系统</h2>
         <p>
-          一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了若依管理系统，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。
+          基于若依框架开发的AI虚拟女友管理系统，提供角色管理、对话功能、用户管理等核心功能。系统采用前后端分离架构，后端基于Spring Boot 3和若依框架，前端基于Vue 2和Element UI，具有完善的权限管理和用户界面。
         </p>
         <p>
           <b>当前版本:</b> <span>v{{ version }}</span>
         </p>
         <p>
-          <el-tag type="danger">&yen;免费开源</el-tag>
+          <el-tag type="success">AI智能管理</el-tag>
         </p>
         <p>
           <el-button
@@ -18,15 +18,15 @@
             size="mini"
             icon="el-icon-cloudy"
             plain
-            @click="goTarget('https://gitee.com/y_project/RuoYi-Vue')"
-            >访问码云</el-button
+            @click="goTarget('https://github.com/lianghongbin/candy-ai-server')"
+            >访问GitHub</el-button
           >
           <el-button
             size="mini"
             icon="el-icon-s-home"
             plain
-            @click="goTarget('http://ruoyi.vip')"
-            >访问主页</el-button
+            @click="goTarget('https://github.com/lianghongbin/candy-ai-server')"
+            >项目主页</el-button
           >
         </p>
       </el-col>
@@ -70,37 +70,24 @@
       <el-col :xs="24" :sm="24" :md="12" :lg="8">
         <el-card class="update-log">
           <div slot="header" class="clearfix">
-            <span>联系信息</span>
+            <span>项目信息</span>
           </div>
           <div class="body">
             <p>
-              <i class="el-icon-s-promotion"></i> 官网：<el-link
-                href="http://www.ruoyi.vip"
+              <i class="el-icon-s-promotion"></i> 项目地址：<el-link
+                href="https://github.com/lianghongbin/candy-ai-server"
                 target="_blank"
-                >http://www.ruoyi.vip</el-link
+                >https://github.com/lianghongbin/candy-ai-server</el-link
               >
             </p>
             <p>
-              <i class="el-icon-user-solid"></i> QQ群：<s> 满937441 </s> <s> 满887144332 </s>
-              <s> 满180251782 </s> <s> 满104180207 </s> <s> 满186866453 </s> <s> 满201396349 </s>
-              <s> 满101456076 </s> <s> 满101539465 </s> <s> 满264312783 </s> <s> 满167385320 </s> 
-              <s> 满104748341 </s> <s> 满160110482 </s> <s> 满170801498 </s> <s> 满108482800 </s> 
-              <s> 满101046199 </s> <s> 满136919097 </s> <s> 满143961921 </s> <s> 满174951577 </s> 
-              <s> 满161281055 </s> <s> 满138988063 </s> <s> 满151450850 </s> <s> 满224622315 </s>
-              <s> 满287842588 </s> <s> 满187944233 </s> <s> 满228578329 </s> <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=GsOo-OLz53J8y_9TPoO6XXSGNRTgbFxA&authKey=R7Uy%2Feq%2BZsoKNqHvRKhiXpypW7DAogoWapOawUGHokJSBIBIre2%2FoiAZeZBSLuBc&noverify=0&group_code=191164766" target="_blank">191164766</a>
+              <i class="el-icon-document"></i> 技术栈：Spring Boot 3 + Vue 2 + Element UI
             </p>
             <p>
-              <i class="el-icon-chat-dot-round"></i> 微信：<a
-                href="javascript:;"
-                >/ *若依</a
-              >
+              <i class="el-icon-info"></i> 基于框架：若依(RuoYi) v3.9.0
             </p>
             <p>
-              <i class="el-icon-money"></i> 支付宝：<a
-                href="javascript:;"
-                class="支付宝信息"
-                >/ *若依</a
-              >
+              <i class="el-icon-star-on"></i> 开源协议：MIT License
             </p>
           </div>
         </el-card>
@@ -1059,7 +1046,7 @@ export default {
   data() {
     return {
       // 版本号
-      version: "3.9.0"
+      version: "1.0.1"
     }
   },
   methods: {
